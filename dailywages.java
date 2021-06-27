07155ccdec42f4a84292fa495f87335647d01599
Your Name <rahuldeshpande93@gmail.com>
2021-06-27 21:57:25 +0530

    uc2(1)

---
+++ dailywages.java
@@ -0,0 +1,9 @@
+public class dailywages {
+public static void main(String[] args)
+{ 
+     int  wageperhour=20; 
+     int fulldayhour=8;
+     int dailywages =wageperhour * fulldayhour; 
+   System.out.println("daily wages is : "+dailywages);
+  }
+}
