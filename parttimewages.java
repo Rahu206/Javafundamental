8ad8a07e506829340c8d4655974c9c4933246676
Your Name <rahuldeshpande93@gmail.com>
2021-06-27 23:37:43 +0530

    UC3

---
+++ parttimewages.java
@@ -0,0 +1,12 @@
+public class parttimewages
+{
+		public static void main(String[] args)
+		{
+	 int wageperhour=20;
+          int fulldayhours=8;
+        int parttimehours=8;
+          int emmployeetotalwage=(wageperhour*fulldayhours)+(wageperhour*parttimehours);
+	System.out.println("parttimewages is: "+emmployeetotalwage);
+	}
+	}
+
